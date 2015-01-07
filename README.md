3e71f4d1e1d6cb3ab2ae7a1f43d3ab9a4f9ad2bc
loweherz <mirkomancin90@gmail.com>
2015-01-07 11:50:45 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,146 @@
 # RPi-LedSPI
 Raspberry Pi LED SPI
+
+# Integrazione di Super Led APA su piattaforma Raspberry PI
+### Fase 1 - Raccolta documentazione su Raspberry e SPI
+ - Understanding SPI on Raspberry -> WiringPi
+ - https://projects.drogon.net/raspberry-pi/wiringpi/spi-library/
+
+WiringPi è una libreria ad-hoc per Raspberry PI che si basa su chiamate di Sistema native Linux. In particolare la libreria dedicata all’SPI interagisce con i device nativi SPI della Raspberry PI.
+I driver kernel della periferica SPI forniscono accesso all'interfaccia SPI BCM2835.
+
+In comune con molti altri driver di periferica rende disponibile dei files nella cartella /dev/ del sistema che permettono la scrittura e la lettura su essi con comandi ioctl. Il modulo HiPi::Device::SPI supporta la comunicazione con i dispositivi SPI utilizzando il driver del kernel.
+Si possono avere i driver di periferica caricati all'avvio del sistema (nel caso mancassero), come descritto nella seguente guida http://raspberrypi.znix.com/hipidocs/topic_spidev.htm.
+
+È anche possibile caricare i driver di periferica in modo dinamico con i comandi:
+- modprobe spi_bcm2708
+- modprobe spidev
+
+Il vantaggio di caricamento all'avvio di questi moduli, è che si possono configurare i driver per consentire agli utenti non root di accedere ai file di dispositivo. Queste impostazioni di configurazione si applicano anche quando si utilizza modprobe ma il processo avrebbe bisogno dei permessi di root.
+
+Il responsabile del dispositivo su Linux si chiama udev e si possono fornire regole per udev che controllano i permessi sui file di periferica che vengono creati.
+I driver del kernel in Raspbian forniscono l'accesso alle periferiche SPI andando a modificare un apposito file chiamato raspi-blacklist.conf. Commentando la riga blacklist spi-bcm2708 (dalla guida) si abilita la periferica voluta:
+```sh
+$ cat /etc/modprobe.d/raspi-blacklist.conf
+```
+```sh
+#blacklist spi and i2c by default (many users don't need them)
+#blacklist spi-bcm2708
+blacklist i2c-bcm2708
+```
+
+Il Raspberry Pi GPIO offre 2 SPI selezionabili con gli appositi pin e il driver di dispositivo funziona fornendo un file diverso per ogni selezione. 
+```sh
+static char *spiDev0 = "/dev/spidev0.0" ;
+static char *spiDev1 = "/dev/spidev0.1" ;
+```
+
+### Fase 2 - Test dell’interfaccia SPI
+Si è settata l’interfaccia SPI tramite la libreria sopra descritta in modo da mantenere sia un buono strato applicativo ma anche garantendo un interfacciamento diretto alle componenti core di Linux.
+
+#####WIRINGPI
+La libreria WiringPi è facilmente scaricabile da github.
+
+Se non si dispone di GIT installato, sotto una delle versioni Debian (per esempio Raspbian), è possibile installarlo con:
+```sh
+sudo apt-get install git-core
+```
+
+Se si ottengono degli errori, assicurarsi che la raspberry sia aggiornata con le ultime versioni di Raspbian:
+```sh
+sudo apt-get update
+sudo apt-get upgrade
+```
+
+Per ottenere WiringPi usando GIT:
+
+```sh
+git clone git: //git.drogon.net/wiringPi
+```
+
+Per accedere al contenuto della cartella scaricata invece e installare la libreria dare i seguenti comandi:
+```sh
+cd wiringPi
+./build
+```
+
+Il nuovo script build compila e installa tutto per voi.
+
+WiringPi include una libreria che può rendere più facile l'utilizzo dell'interfaccia SPI del Raspberry Pi.
+
+In primo luogo, è necessario utilizzare l'utility GPIO per caricare i driver SPI nel kernel:
+```sh
+gpio load spi
+```
+Se avete bisogno di una dimensione di buffer maggiore di 4KB, è possibile specificare la dimensione (in KB) sulla riga di comando:
+```sh
+gpio load spi 100
+```
+assegnerà un buffer  di 100 KB. (Non dovrebbe essere necessario questo. Di default è più che sufficiente per la maggior parte delle applicazioni).
+
+Per utilizzare la libreria SPI, è necessario includere la libreria nel vostro programma:
+```sh
+#include <wiringPiSPI.h>
+```
+
+######Funzioni disponibili:
+
+```sh
+int wiringPiSPISetup (int canale, velocità int);
+```
+Questo è il modo per inizializzare un canale (Pi L'ha 2 canali, 0 e 1). Il parametro velocità è un numero intero compreso tra 500.000 e 32.000.000 attraverso rappresenta la velocità di clock SPI in Hz.
+
+Il valore restituito è il Linux file descrittore per il dispositivo, o -1 in caso di errore. Se è avvenuto un errore, è possibile utilizzare lo standard errno variabile globale per capire perché.
+```sh
+int wiringPiSPIDataRW (int canale, unsigned char * dati, int len);
+```
+Esegue una scrittura simultanea / transazione leggere sul bus SPI selezionato. I dati che era nel buffer vengono sovrascritti dai dati restituiti dal bus SPI.
+
+###I programmi devono essere linkati con -lwiringPi.
+<br>
+
+##Codice di prova
+```sh
+#include <wiringPiSPI.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int chan=1;
+    int speed=1000000;
+
+    if( wiringPiSPISetup (chan, speed)==-1)
+    {
+    	printf("Could not initialise SPI\n");
+       return(0);
+    }
+     
+    printf("When ready hit enter.\n");
+    (void) getchar();// remove the CR
+    unsigned char buff[100];
+
+    while (1)
+    {
+        printf("Input a string, Input 0 to finish : ");
+        gets (buff);
+        if(buff[0]=='0')
+        {
+            break;
+        }
+        else
+        {
+            int ret=wiringPiSPIDataRW (chan,buff,100) ;
+            printf ("%s \n", buff);
+        } 
+    } 
+}
+```
+Per compilare il sorgente basta dare il seguente comando:
+```sh
+gcc –o prova prova.c –lwiringPi
+```
+
+
+
+
+
---
+++ source/demo.c
@@ -0,0 +1,265 @@
+#include<stdio.h>
+#include <wiringPiSPI.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+void initBuff();
+void milliSleep(int);
+int kbhit(void);
+unsigned char buff[88];
+int num_state[4];
+
+int main()
+{
+     int chan=1;
+     int speed=512000;
+     int i;
+     char state;
+     int seq_state;
+     int up=1;
+
+     if( wiringPiSPISetup (chan, speed)==-1)
+     {
+         printf("Could not initialise SPI\n");
+         return(0);
+     }
+     printf("When ready hit enter.\n");
+     (void) getchar();// remove the CR
+
+     initBuff();
+     printf("- 'a' demo alternato\n");
+     printf("- 'r' red\n");
+     printf("- 'g' green\n");
+     printf("- 'b' blue\n");
+     printf("- 'm' misto\n");
+     printf("- 'm' misto\n");
+	 printf("- 'p' demo pong")
+     printf("- 'q' turn off\n");
+     printf("- '0' quit\n\n\n");
+
+
+     while(1)
+     {
+
+         if (kbhit())
+         {
+            state = getc(stdin);
+            seq_state=0;
+         }
+
+         switch(state)
+         {
+                case'a': //R-G-B
+                    if (seq_state == 0)
+                    {
+                        //RED
+                        for(i = 0; i<20;i++)
+                        {
+                            buff[4*i+4]=0xf0;
+                            buff[4*i+5]=0;
+                            buff[4*i+6]=0;
+                            buff[4*i+7]=255;
+                        }
+                        seq_state++;
+                        wiringPiSPIDataRW (chan,buff,88);
+                    }
+                    else if (seq_state == 1)
+                    {
+                        //GREEN
+                        for(i = 0; i<20;i++)
+                        {
+                            buff[4*i+4]=0xf0;
+                            buff[4*i+5]=0;
+                            buff[4*i+6]=255;
+                            buff[4*i+7]=0;
+                        }
+                        seq_state++;
+                        wiringPiSPIDataRW (chan,buff,88);
+                    }
+                     else if (seq_state == 2)
+                    {
+                        //BLUE
+                        for(i = 0; i<20;i++)
+                        {
+                            buff[4*i+4]=0xf0;
+                            buff[4*i+5]=255;
+                            buff[4*i+6]=0;
+                            buff[4*i+7]=0;
+                        }
+                        seq_state=0;
+                        wiringPiSPIDataRW (chan,buff,88);
+                    }
+                    milliSleep(1000);
+                    break;
+                case'r':
+                    for(i = 0; i<20;i++)
+                        {
+                            buff[4*i+4]=0xf0;
+                            buff[4*i+5]=0;
+                            buff[4*i+6]=0;
+                            buff[4*i+7]=255;
+                        }
+                        wiringPiSPIDataRW (chan,buff,88);
+                    break;
+				case'g':
+                    for(i = 0; i<20;i++)
+                        {
+                            buff[4*i+4]=0xf0;
+                            buff[4*i+5]=0;
+                            buff[4*i+6]=255;
+                            buff[4*i+7]=0;
+                        }
+                        wiringPiSPIDataRW (chan,buff,88);
+                    break;
+                case'b':
+                    for(i = 0; i<20;i++)
+                        {
+                            buff[4*i+4]=0xf0;
+                            buff[4*i+5]=255;
+                            buff[4*i+6]=0;
+                            buff[4*i+7]=0;
+                        }
+                        wiringPiSPIDataRW (chan,buff,88);
+                    break;
+				case'm':
+                    for(i = 0; i<20;i++)
+                    {
+                         buff[4*i+4]=0xf0;
+                         if(i%3==0)
+                         {
+                             buff[4*i+5]=0;
+                             buff[4*i+6]=0;
+                             buff[4*i+7]=255;
+                         }
+                         else if(i%3==1)
+                         {
+                             buff[4*i+5]=0;
+                             buff[4*i+6]=255;
+                             buff[4*i+7]=0;
+                         }
+                         else if(i%3==2)
+                         {
+                             buff[4*i+5]=255;
+                             buff[4*i+6]=0;
+                             buff[4*i+7]=0;
+                         }
+                    }
+                    wiringPiSPIDataRW (chan,buff,88);
+                    break;
+				case'p':
+                    for(i = 0; i<20;i++)
+                    {
+                          buff[4*i+4]=0xf0;
+                          buff[4*i+5]=0;
+                          buff[4*i+6]=0;
+                          buff[4*i+7]=0;
+                    }
+                    buff[4*seq_state+5]=255;
+                    buff[4*seq_state+6]=255;
+                    buff[4*seq_state+7]=255;
+
+                    if ((seq_state<=19)&&(up==1))
+                    {
+                        if (seq_state<19) seq_state++;
+                        else up=0;
+                    }
+                    if ((seq_state>=0)&&(up==0))
+                    {
+                        if (seq_state>0) seq_state--;
+                        else up=1;
+                    }
+                    wiringPiSPIDataRW (chan,buff,88);
+                    milliSleep(100);
+                    break;
+                case 'q':
+                    //TURN OFF
+                    for(i = 0; i<20;i++)
+                    {
+                         buff[4*i+4]=0xf0;
+                         buff[4*i+5]=0;
+                         buff[4*i+6]=0;
+                         buff[4*i+7]=0;
+                    }
+                    wiringPiSPIDataRW (chan,buff,88);
+                    break;
+                case '0':
+                    //TURN OFF
+                    for(i = 0; i<20;i++)
+                    {
+                         buff[4*i+4]=0xf0;
+                         buff[4*i+5]=0;
+                         buff[4*i+6]=0;
+                         buff[4*i+7]=0;
+                    }
+                    wiringPiSPIDataRW (chan,buff,88);
+                    return(0);
+                    break;
+
+
+         }
+     }
+}
+
+
+void initBuff()
+{
+    int i;
+
+    buff[0]=0;
+    buff[1]=0;
+    buff[2]=0;
+    buff[3]=0;
+
+    buff[87]=255;
+    buff[86]=255;
+    buff[85]=255;
+    buff[84]=255;
+}
+
+int kbhit(void)
+{
+   struct termios oldt, newt;
+   int ch;
+   int oldf;
+
+   tcgetattr(STDIN_FILENO, &oldt);
+   newt = oldt;
+   newt.c_lflag &= ~(ICANON | ECHO);
+   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+   oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+   fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+   ch = getchar();
+
+   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+   fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+   if(ch != EOF)
+   {
+      ungetc(ch, stdin);
+      return 1;
+   }
+
+   return 0;
+}
+
+void milliSleep(int millisec)
+{
+        int i;
+        for (i=0;i<millisec;i++)
+        {
+                usleep(1000);
+        }
+}
+
+
+
